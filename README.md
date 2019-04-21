0f9e5e67f84ccc6dc4155cd86b848bd6728747de
freewind <nowindlee@gmail.com>
2019-04-21 23:30:21 +0800

    demo

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
-TypeScript Cypress Test Check DOM Demo
-=========================================
+TypeScript Cypress Click On A Button and Wait Element Appear Demo
+==================================================================
+
+`cy.get`本身具有wait的功能，还可以指定timeout.
+
+注意: `cy.wait`是单纯的等待一定时间，或者某个请求结束。
 
 ```
 npm install
--- cypress/integration/hello_spec.ts
+++ cypress/integration/hello_spec.ts
@@ -1,10 +1,11 @@
 describe('DOM check', () => {
   beforeEach(() => {
-    cy.visit('https://example.cypress.io/');
+    cy.visit('https://example.cypress.io/commands/waiting');
   })
 
   it('exist', () => {
-    cy.get('.home-list').should('exist');
+    cy.get('button.network-btn').click();
+    cy.get('div.network-comment', {timeout: 3000}).should('exist');
   })
 
 })
